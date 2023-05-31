e655d113b8558b855d011c9b0f6c21ad19cb2328
Moses Muchoki <mosesmmuchoki@gmail.com>
2023-06-01 00:14:03 +0300

    Generated Student Model

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Student;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -15,13 +16,15 @@ class WelcomeController extends Controller
         //return view('welcome');
 
        // 1. Using Raw SQL QUeries
-        $users=DB::Select('select * from users');
-        dd($users);
+        // $users=DB::Select('select * from users');
+        // dd($users);
        //2. Using Query builder
         //   $users=DB::table('users')->select(['name','email'])->whereNotNull('email')->orderBy('name')->get();
         //   dd($users);
 
         //3. Eloquent ORM
+        $students= Student::all();
+        dd($students);
 
     }
 }
