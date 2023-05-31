73f69bafb22b0d4db71fe7e558c2a28026d18629
Moses Muchoki <mosesmmuchoki@gmail.com>
2023-05-31 23:36:26 +0300

    modified user table

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -3,8 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class WelcomeController extends Controller
 {
     //
+    public function index(){
+    
+    
+ 
+        //return view('welcome');
+
+       // 1. Using Raw SQL QUeries
+        // $users=DB::Select('select * from users');
+        // dd($users);
+       //2. Using Query builder
+          $users=DB::table('users')->select(['name','email'])->whereNotNull('email')->orderBy('name')->get();
+          dd($users);
+
+        //3. Eloquent ORM
+
+    }
 }
